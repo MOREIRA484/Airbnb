42c33b6dded8a775e07db644376b461394ee1a5a
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-19 16:28:46 -0300

    codigo listaItem

--- Airbnb/Core/Listings/ListingItemView.swift
+++ Airbnb/Core/Listings/ListingItemView.swift
@@ -8,8 +8,60 @@
 import SwiftUI
 
 struct ListingItemView: View {
+    
+    var images = [
+        "listing-1",
+        "listing-2",
+        "listing-3",
+        "listing-4",
+    ]
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 8) {
+            // IMages
+            TabView {
+                ForEach(images, id: \.self) { image in
+                    
+                    Image(image)
+                        .resizable()
+                        .scaledToFill()
+                }
+            }
+                .frame(height: 320)
+                .cornerRadius(10)
+                .tabViewStyle(.page)
+            
+            // listing details
+            
+            HStack(alignment: .top){
+                // Details
+                VStack(alignment: .leading) {
+                  
+                        Text("Miami, Florida ")
+                            .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundColor(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    Text("$567 night")
+                        .fontWeight(.semibold)
+                }
+                
+                Spacer()
+                // Rating
+                HStack(spacing: 2) {
+                    
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+        }
+        .padding()
     }
 }
 
