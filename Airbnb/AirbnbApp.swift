11e1380959595cfcd8f6e29cbbaf3faf39ef74f9
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-19 15:32:28 -0300

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
