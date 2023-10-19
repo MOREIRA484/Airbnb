9b3fec204eb99f0f628f4bfcf987b603dbe5fb6a
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-19 16:42:30 -0300

    Fiz minha searchBar e acrescentei ela na minha ExploreView

---
+++ Airbnb/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 19/10/23.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
