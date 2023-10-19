240e2813810b782bbfc0b6d3f7f6328c3e647b77
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-19 16:27:23 -0300

    ExploreView Feita e a extrutura da Imagem (nome, local, estrelas, e valor).. por enquanto

---
+++ Airbnb/Core/Listings/ListingItemView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingView.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 19/10/23.
+//
+
+import SwiftUI
+
+struct ListingItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingItemView()
+}
