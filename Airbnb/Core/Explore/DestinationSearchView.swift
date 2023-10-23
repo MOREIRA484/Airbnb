276346bcdbc9c225d728978b8809c6dacf56aa8d
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-23 18:55:56 -0300

    DestinationSearchView / colocando animação na searchBar 1/2

---
+++ Airbnb/Core/Explore/DestinationSearchView.swift
@@ -0,0 +1,18 @@
+//
+//  DestinationSearchView.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 23/10/23.
+//
+
+import SwiftUI
+
+struct DestinationSearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DestinationSearchView()
+}
