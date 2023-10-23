177f42b8654323758ce8988f86baf492d0060885
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-23 17:12:45 -0300

    FIZ UMA ListingDetail, parte da reserva !, acrescentei uma foto minha

---
+++ Airbnb/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 19/10/23.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
