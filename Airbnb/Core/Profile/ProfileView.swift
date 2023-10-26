090e0b59d8382248f53d1750fb50bbfa10d0f7f9
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-26 18:00:20 -0300

    codigo

--- Airbnb/Core/Profile/ProfileView.swift
+++ Airbnb/Core/Profile/ProfileView.swift
@@ -9,7 +9,46 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        //Profile login View
+        VStack(alignment: .leading, spacing: 10) {
+            Text("Profile")
+                .font(.title)
+                .fontWeight(.semibold)
+            
+            Text("Log in to start planning your next trip")
+                .font(.subheadline)
+            
+            Button {
+                //
+            } label: {
+                Text ("Log in")
+                    .frame(width: 350, height: 46)
+                    .foregroundStyle(.white)
+                    .background(.pink)
+                    .cornerRadius(12)
+                    .padding(.top)
+                    
+            }
+            HStack {
+                Text("dont't have as account ?")
+                
+                Text("Sing Up")
+                    .fontWeight(.semibold)
+                    .underline()
+                    
+            }
+            .font(.caption)
+        }
+        
+        
+        //Profile options
+        VStack(spacing: 24) {
+            ProfileOptionRowView(imageName: "gear", title: "Setting")
+            ProfileOptionRowView(imageName: "gear", title: "Accessibility")
+            ProfileOptionRowView(imageName: "questionmark.circle", title: "visit the help center")
+        }
+        .padding(.vertical)
     }
 }
 
