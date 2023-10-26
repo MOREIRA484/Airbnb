eed84acf11ac1c8ed42255802bd35dc75f0a9ab9
leonardo <leonardoazevedomoreiraa@gmail.com>
2023-10-26 17:59:51 -0300

    parte do perfil e as opção que tem nele !

---
+++ Airbnb/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileOptionRowView.swift
+//  Airbnb
+//
+//  Created by leonardo Moreira on 26/10/23.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileOptionRowView()
+}
